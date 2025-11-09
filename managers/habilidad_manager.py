0388956b58ab5a267b60fec69d84d0e92f1ca79b
Benjagamallo08 <benjamingamallo8@gmail.com>
2025-11-09 19:15:58 -0300

    Ahora si anda

--- managers/habilidad_manager.py
+++ managers/habilidad_manager.py
@@ -6,6 +6,6 @@ from modelos import habilidad_model
 class manager_habilidad:
      def agregar_habilidad (self, Habilidad:habilidad_model, cursor:psycopg.Cursor):
        
-       cursor.execute("INSERT INTO habilidad (habilidad) VALUES=(%s)", (Habilidad.habilidad))
+       cursor.execute("INSERT INTO habilidad (habilidad) VALUES (%s)", (Habilidad.habilidad,),)
 
        return ("!!habilidad agregada!!")  
