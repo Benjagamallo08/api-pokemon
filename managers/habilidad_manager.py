0a0a824bb53a912a6b4812ee97459c66001f6b97
Benjagamallo08 <benjamingamallo8@gmail.com>
2025-11-09 19:11:16 -0300

    todavia no anda

--- managers/habilidad_manager.py
+++ managers/habilidad_manager.py
@@ -4,7 +4,8 @@ from modelos import habilidad_model
 
 
 class manager_habilidad:
-    def agregar_habilidad (self, habilidad:habilidad_model, cursor:psycopg.Cursor):
-       cursor.execute("INSERT INTO habilidad (habilidad) VALUES=(%s)", (habilidad.habilidad))
+     def agregar_habilidad (self, Habilidad:habilidad_model, cursor:psycopg.Cursor):
+       
+       cursor.execute("INSERT INTO habilidad (habilidad) VALUES=(%s)", (Habilidad.habilidad))
 
        return ("!!habilidad agregada!!")  
--- routes/router_habilidad.py
+++ routes/router_habilidad.py
@@ -1,16 +1,17 @@
+import psycopg
 from fastapi import APIRouter , Depends
 from managers.conexion import getCursor
 from managers.habilidad_manager import manager_habilidad
 from modelos import habilidad_model
-import psycopg
+
 
 habilidad_manager = manager_habilidad ()
 
-router= APIRouter (prefix="/habilidad" , tags= ["habilidad routes"])
+router= APIRouter (prefix= "/habilidad" , tags= ["habilidad routes"])
 
 @router.post ("/agegar_habilidad")
-def habilidad_post (habilidad:habilidad_model, cursor: psycopg.Cursor = Depends (getCursor)):
-    return habilidad_manager.agregar_habilidad (habilidad, cursor)
+def habilidad_post (Habilidad: habilidad_model , cursor: psycopg.Cursor = Depends (getCursor)):
+    return habilidad_manager.agregar_habilidad (Habilidad, cursor)
 
 
 
